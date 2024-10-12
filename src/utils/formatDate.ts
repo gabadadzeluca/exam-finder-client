097d56099296bcd4ed990b8ae7d824e960dae251
Luca Gabadadze <gabadadzeluca@gmail.com>
2024-10-12 17:42:20 +0400

    add formatDate function

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,11 @@
+export const formatDate = (dateStr:Date) => {
+  const date = new Date(dateStr);
+
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+  const year = date.getFullYear();
+
+  return `${hours}:${minutes} ${day}/${month}/${year}`;
+};
