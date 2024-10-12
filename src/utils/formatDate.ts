6de485a1cb82526eefe80c9dbae5ede821473ec4
Luca Gabadadze <gabadadzeluca@gmail.com>
2024-10-13 01:33:17 +0400

    update formatDate function so it doesnt return NaN anymore

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,11 +1,30 @@
-export const formatDate = (dateStr:Date) => {
+export const formatDate = (dateStr: Date) => {
   const date = new Date(dateStr);
 
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+  // Check if the date is valid
+  if (isNaN(date.getTime())) {
+    return null;
+  }
+
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const day = date.getDate();
+  const month = date.getMonth() + 1; // months are zero-based
   const year = date.getFullYear();
 
-  return `${hours}:${minutes} ${day}/${month}/${year}`;
+  // Check if any of the values are NaN
+  if (
+    isNaN(hours) ||
+    isNaN(minutes) ||
+    isNaN(day) ||
+    isNaN(month) ||
+    isNaN(year)
+  ) {
+    return null;
+  }
+
+  return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+    2,
+    "0"
+  )} ${String(day).padStart(2, "0")}/${String(month).padStart(2, "0")}/${year}`;
 };
