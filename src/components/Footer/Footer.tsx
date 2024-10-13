2ea62fb0d41ad859c560d3ebdd146bb294e9cbf4
Luca Gabadadze <gabadadzeluca@gmail.com>
2024-10-14 00:37:56 +0400

    add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+import { SLogo } from "../../App.styled"
+import githubLogo from "../../assets/svgs/githubLogo.svg";
+
+const GITHUB_LINK = "https://github.com/gabadadzeluca/exam-finder-client";
+
+
+export const Footer = () =>{
+  return(
+    <SFooterDiv>
+      <a href={GITHUB_LINK} target="_blank" rel="noopener noreferrer">
+        <SLogo $logo={githubLogo} width="2rem"/>
+      </a>
+    </SFooterDiv>
+  )
+}
+
+const SFooterDiv = styled.div`
+  width: 95%;
+  height: 3rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+`
