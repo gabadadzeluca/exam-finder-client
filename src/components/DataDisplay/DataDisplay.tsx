6cd32f6a3c9e3d083fd7ec573482c3f4aeb45223
Luca Gabadadze <gabadadzeluca@gmail.com>
2024-10-13 18:44:36 +0400

    add starting styles to dataDisplayt

--- src/components/DataDisplay/DataDisplay.tsx
+++ src/components/DataDisplay/DataDisplay.tsx
@@ -1,18 +1,46 @@
+import styled from "styled-components";
+import { COLORS } from "../../utils/colors";
+
 export const DataDisplay = (props: { examData: any[][] }) => {
   const examData = props.examData;
   console.log("DATA(DISPL):", examData);
   if (examData != null && examData.length > 0) {
     return (
-      <div>
+      <SDataContainer>
         {examData.map((arr, rowIndex) => (
-          <div key={rowIndex}>
+          <SDataRowDiv key={rowIndex}>
             {arr.map((value: string, colIndex: number) => (
               <div key={colIndex}>{value}</div>
             ))}
-          </div>
+          </SDataRowDiv>
         ))}
-      </div>
+      </SDataContainer>
     );
   }
   return null;
 };
+
+
+const SDataContainer = styled.div`
+  background-color: #101010;
+  width: 70%;
+  align-self: flex-start;
+  margin-left: 3rem;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+
+`
+
+const SDataRowDiv = styled.div`
+  display: flex;  
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  
+  margin: 2rem 0; // TEMPORARY
+
+  height: 5rem;
+  background-color: blue; // TEMP
+
+`
