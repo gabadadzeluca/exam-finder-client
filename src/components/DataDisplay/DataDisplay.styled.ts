f4ccad1c7e2e7c9b2de0a469f3ba5a714b9e5863
Luca Gabadadze <gabadadzeluca@gmail.com>
2024-10-13 20:37:59 +0400

    add some media querries

--- src/components/DataDisplay/DataDisplay.styled.ts
+++ src/components/DataDisplay/DataDisplay.styled.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 export const SDataContainer = styled.div`
-background-color: #101010;
+  background-color: #101010;
   width: 70%;
   display: flex;
   flex-direction: column;
@@ -9,6 +9,16 @@ background-color: #101010;
   margin-left: 3rem;
   font-size: 18px;
   font-weight: 500;
+
+  @media (max-width: 992px) {
+    width: 75%;
+    margin-left: 0;
+    align-self: center;
+  }
+
+  @media (max-width: 768px) {
+    width: 90%;
+  }
 `;
 
 export const SDataRowDiv = styled.div`
@@ -16,9 +26,23 @@ export const SDataRowDiv = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  padding: 0 2rem;
 
   margin: 0 0 2rem; // TEMPORARY
+  @media (max-width: 1200px) {
+    padding: 0 2rem;
+  }
+
+  @media (max-width: 992px) {
+    padding: 0 1.5rem;
+  }
+
+  @media (max-width: 768px) {
+    padding: 0 1rem;
+  }
+
+  @media (max-width: 576px) {
+    padding: 0 0.7rem;
+  }
 
   min-height: 6rem;
   background-color: gray; // TEMP
@@ -35,12 +59,31 @@ export const SDropdownDataDiv = styled.div`
   padding: 0 10px;
 
   border: 2px solid white; // TEMP
-
 `;
 
 // data paragraph in the dropdown
-export const SDataP = styled.p``
+export const SDataP = styled.p`
+  margin: 0.5rem 1rem;
+  font-size: 0.9375rem;
+`;
 
 export const SInlineDataP = styled.p`
   max-width: 12rem;
-`
+  font-size: 1.25rem;
+
+  @media (max-width: 1200px) {
+    font-size: 1.15rem;
+  }
+
+  @media (max-width: 992px) {
+    font-size: 1rem;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 0.875rem;
+  }
+
+  @media (max-width: 576px) {
+    font-size: 0.75rem;
+  }
+`;
