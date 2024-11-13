2ca2da14ee04197ea35a97d3134f91f2db5e3ee2
Luca Gabadadze <gabadadzeluca@gmail.com>
2024-11-13 21:15:56 +0400

    add cat component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import { SLogo } from "../../App.styled"
 import githubLogo from "/assets/svgs/githubLogo.svg";
+import catImg from "/assets/images/cat.png";
 
 const GITHUB_LINK = "https://github.com/gabadadzeluca/exam-finder-client";
 
@@ -11,6 +12,7 @@ export const Footer = () =>{
       <a href={GITHUB_LINK} target="_blank" rel="noopener noreferrer">
         <SLogo $logo={githubLogo} width="2rem"/>
       </a>
+      <SCat catImg={catImg} />
     </SFooterDiv>
   )
 }
@@ -24,4 +26,35 @@ const SFooterDiv = styled.div`
   position: fixed;
   bottom: 0;
   left: 0;
+`
+const SCat = styled.div<{catImg: string}>`
+  width: 12rem;
+  height: 12rem;
+  background-image: url(${({ catImg }) => catImg});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  position: absolute;
+  bottom: -0.75rem;
+  left: 1rem;
+
+  @media (max-width: 1200px) {
+    width: 12rem;
+    height: 12rem;
+  }
+
+  @media (max-width: 992px) {
+    width: 10rem;
+    height: 10rem;
+  }
+
+  @media (max-width: 768px) {
+    width: 8rem;
+    height: 8rem;
+  }
+
+  @media (max-width: 576px) {
+    width: 6rem;
+    height: 6rem;
+  }
 `
